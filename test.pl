c1315cceeac26eb4f8b4c1ee642a1c90d362c6e8
Sama Ahmed <102187124+SamaAhmedS@users.noreply.github.com>
2024-05-01 21:13:04 +0300

    A star algorithm try1

--- test.pl
+++ test.pl
@@ -17,10 +17,11 @@ board1([
 ]).
 
 % moves 
-move([0, 1]). % right
-move([1, 0]). % down
-move([0, -1]). % left
-move([-1, 0]). % up
+move(0, 1). % right
+move(-1, 0). % up
+move(0, -1). % left
+move(1, 0). % down
+
 
 % check if the cell is in the board
 inBoard(X, Y, N, M) :- X >= 0, X < N, Y >= 0, Y < M.
@@ -28,19 +29,15 @@ inBoard(X, Y, N, M) :- X >= 0, X < N, Y >= 0, Y < M.
 % check if the cell is not visited
 notVisited(X, Y, Visited) :- \+ member([X, Y], Visited).
 
-% check if the cell has the same color
-sameColor(X, Y, Color, Board) :- nth0(X, Board, Row), nth0(Y, Row, Color).
-
 % check if the last node is adjacent to the first node
 adjacent([X1, Y1], [X2, Y2]) :- 
     X1 =:= X2, abs(Y1 - Y2) =:= 1;
     Y1 =:= Y2, abs(X1 - X2) =:= 1.
 
 % check if the cycle is valid
-validCycle(Cycle, Color, Board) :- 
+validCycle(Cycle) :- 
     length(Cycle, L), L >= 4,
-    closedCycle(Cycle),
-    forall(member([X, Y], Cycle), sameColor(X, Y, Color, Board)).
+    closedCycle(Cycle).
 
 % check if the cycle is closed
 closedCycle(Cycle) :-
@@ -52,8 +49,8 @@ closedCycle(Cycle) :-
 getColor(X, Y, Color, Board) :- nth0(X, Board, Row), nth0(Y, Row, Color).
 
 % depth first search
-dfs(_, _, Visited, Color, Board, _, _) :- 
-    validCycle(Visited, Color, Board), 
+dfs(_, _, Visited, Color, _, _, _) :- 
+    validCycle(Visited), 
     write('Cycle: '), write(Visited), write(' Color: '), write(Color), nl, 
     !.
 
@@ -69,15 +66,11 @@ dfs(X, Y, Visited, Color, Board, N, M) :-
     dfs(X2, Y2, Visited1, Color, Board, N, M).
 
 
-
-minV([], V, V). % Base case: when the list is empty, V is unified with the accumulator S
-
-minV([H|Tail], S, V) :-
-    S > H, minV(Tail, H, V); minV(Tail, S, V). % If the head H is smaller than S, update S with H and continue recursively
-
-findMinIndex([H|T], Ind, V) :-
-    minV(T, H, V),
-    nth0(Ind, [H|T], V),!. % Find the index of the minimum value V in the List
+% moves 
+moveA([0, 1]). % right
+moveA([-1, 0]). % up
+moveA([0, -1]). % left
+moveA([1, 0]). % down
 
 % filling the 1d list with heuristic values using manhattan distance
 fill_1d_HL([], _, _, _, _, _, _, []).
@@ -96,17 +89,15 @@ fill_2d_HL([Row|Rest], X, Y, GoalX, GoalY, GoalColor, Board, [Row1|Rest1]) :-
     X1 is X + 1,
     fill_2d_HL(Rest, X1, Y, GoalX, GoalY, GoalColor, Board, Rest1).
 
-% x, y is the nood i want to go to Ind is the path form start to the nood from it i will move 
-% to the nood x, y. List has [[visited path]] as each visitedpath is from the start to a nood and the list with total costs
-validMove(X, Y, List, Ind, Color, Board, N, M, NewList):-
-    inBoard(X, Y, N, M),getColor(X, Y, Color, Board),
-    nth0(Ind, List, Visited), notVisited(X, Y, Visited),
-    append(Visited, [[X, Y]], Visited1), 
-    append(List, Visited1 , NewList).
 
+minV([], V, V). % Base case: when the list is empty, V is unified with the accumulator S
+minV([H|Tail], S, V) :-
+    S > H, minV(Tail, H, V); minV(Tail, S, V). % If the head H is smaller than S, update S with H and continue recursively
 
-getallmoves(Moves):-
-    findall(Template, move(Template), Moves).
+findMinIndex([H|T], Ind, V) :-
+    minV(T, H, V),
+    nth0(Ind, [H|T], V),!. % Find the index of the minimum value V in the List
+
 
 % If the goal is achieved, print the result and terminate the process.
 end(LX, LY, Ex, Ey, Visited, Cost, Color) :- 
@@ -118,30 +109,62 @@ remove_at(Index, List, Result) :-
     nth0(Index, List, Element),
     select(Element, List, Result).
 
+getallchild(X,Y, Open, Moves, N, M, Color, Board,  Visited):-
+    findall(Temp, moveA(Temp), List),
+    get_open(List, X, Y, Open, Moves, N, M, Color, Board,  Visited).
+
+get_open([], _, _, Open, Open, _, _, _, _, _).
+get_open([[X1,Y1]|Rest], X, Y, Open, Open2, N, M, Color, Board,  Visited) :-
+    X2 is X + X1,
+    Y2 is Y + Y1,
+    ((inBoard(X2,Y2, N,M), getColor(X2, Y2, Color, Board), notVisited(X2, Y2, Visited) )-> append(Open, [[X2,Y2]], NewOpen) ; NewOpen = Open),
+    get_open(Rest, X, Y, NewOpen, Open2, N, M, Color, Board,  Visited).
+
+
+
+update([], _, Visited, Visited, _, _, ExactCost, ExactCost, EstimatedTCost, EstimatedTCost).
+update([[X, Y]|T], Ind, Visited, UpVisited, Heuristic, Ec, ExactCost, NExactCost, EstimatedTCost, NEstimatedTCost):-
+    nth0(Ind, Visited, LVisited),
+    append(LVisited, [[X, Y]], Visited1),
+    append(Visited, [Visited1] , UpVisited),
+    NEc is Ec + 1, append(ExactCost, [NEc], NExactCost),
+    nth0(X, Heuristic, HL),nth0(Y, HL, HV), NEst is NEc + HV,
+           append(EstimatedTCost, [NEst], NEstimatedTCost),
+    update(T, Ind, UpVisited, _, Heuristic, Ec, NExactCost, _, NEstimatedTCost, _).
+    
+
 % ExactCost has actual total cost from start to the last nood in the visited list
 aStar(Ex, Ey, Visited, ExactCost, EstimatedTCost, Color, Heuristic, Board, N, M) :- 
+   % if Color, starting point color, and goal color are
+    length(Visited, L), L =:= 1, nth0(0, Visited, LVisited), length(LVisited, L1), L1 =:= 1, last(LVisited, [SX, SY]),
+    (\+getColor(SX, SY, Color, Board); \+getColor(Ex, Ey, Color, Board)),
+    write('The starting point, goal, and color you provided do not match'),!;
+    
+    
+    length(EstimatedTCost, L), L < 1, write('Your goal can not be reached'),!;
     findMinIndex(EstimatedTCost, Ind, _), nth0(Ind, ExactCost, Ec),
     nth0(Ind, Visited, LVisited), last(LVisited, [SX, SY]), 
     end(SX, SY, Ex, Ey, LVisited, Ec, Color);  % if this is the Goal stop
     
     findMinIndex(EstimatedTCost, Ind, _), nth0(Ind, ExactCost, Ec),
-    nth0(Ind, Visited, LVisited), last(LVisited, [SX, SY]),  
-    getallmoves(Moves), 
-    forall(member([X, Y], Moves), X1 is SX + X, Y1 is SY + Y,
-           validMove(X1, Y1, Visited, Ind, Color, Board, N, M, NVisited), 
-           NEc is Ec + 1,
-           append(ExactCost, [NEc], NExactCost), nth0(X1, Heuristic, HL),
-           nth0(Y1, HL, HV), NEst is NEc + HV,
-           append(EstimatedTCost, [NEst], NEstimatedTCost)      
-           ),
+    nth0(Ind, Visited, LVisited), last(LVisited, [SX, SY]),
+    getallchild(SX,SY,[],Moves, N, M, Color, Board,LVisited),
+    update(Moves, Ind, Visited, NVisited, Heuristic, Ec, ExactCost, NExactCost, EstimatedTCost, NEstimatedTCost),
+    
     remove_at(Ind, NEstimatedTCost, NewEstimatedTCost),
-    remove_at(Ind, NExactCost, NewExactCost), remove_at(Ind, NVisited, NewVisited),
+    remove_at(Ind, NExactCost, NewExactCost),
+    remove_at(Ind, NVisited, NewVisited),
+   
     aStar(Ex, Ey, NewVisited, NewExactCost, NewEstimatedTCost, Color, Heuristic, Board, N, M).
 
 
+
+
 /** <examples>
-?- dfs(2,0,_,_,[[2,0]],blue,[[yellow, yellow, yellow, red], [blue, yellow, blue, yellow],[blue, blue, blue, yellow],[blue, blue, blue, yellow]],4,4).
-?- aStar(1,5,[[[0,1]]],[0],[5],r, [[-1,5,-1,-1,2,-1],[5,4,3,2,1,0],[-1,-1,-1,3,2,-1]], [[b,r,b,b,r,b],[r,r,r,r,r,r],[b,b,b,r,r,b]], 3, 6)
+dfs(2,0,[[2,0]],blue,[[yellow, yellow, yellow, red], [blue, yellow, blue, yellow],[blue, blue, blue, yellow],[blue, blue, blue, yellow]],4,4).
+aStar(1,5,[[[0,1]]],[0],[5],r, [[-1,5,-1,-1,2,-1],[5,4,3,2,1,0],[-1,-1,-1,3,2,-1]], [[b,r,b,b,r,b],[r,r,r,r,r,r],[b,b,b,r,r,b]], 3, 6)
+aStar(1,4,[[[0,0]]],[0],[5],r, [[-1,4,-1,-1,2,-1],[4,3,2,1,0,1],[-1,-1,-1,2,1,-1]], [[b,r,b,b,r,b],[r,r,r,r,r,r],[b,b,b,r,r,b]], 3, 6)
+   
 */
 
 
